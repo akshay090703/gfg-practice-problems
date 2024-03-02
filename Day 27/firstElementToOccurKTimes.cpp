5e0f9bb743a720a9f7fdbf173ab70d2887af00bb
Akshay <akshay.pandey2021@vitbhopal.ac.in>
2024-03-02 22:09:49 +0530

    Day 27 problem

---
+++ Day 27/firstElementToOccurKTimes.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    int firstElementKTime(int n, int k, int a[])
+    {
+        // Occurances of the elements
+        unordered_map<int, int> occurances;
+
+        for (int i = 0; i < n; i++)
+        {
+            occurances[a[i]]++;
+
+            if (occurances[a[i]] == k)
+            {
+                return a[i];
+            }
+        }
+
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        Solution ob;
+        cout << ob.firstElementKTime(n, k, a) << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
