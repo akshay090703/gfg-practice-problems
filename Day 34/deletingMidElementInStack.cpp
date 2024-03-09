2810fd48283885cccb8961192a368441d21bfada
Akshay Kumar Pandey <92268742+akshay090703@users.noreply.github.com>
2024-03-09 20:21:37 +0530

    Create deletingMidElementInStack.cpp

---
+++ Day 34/deletingMidElementInStack.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
+    public:
+    //Function to delete middle element of a stack.
+    void solve(stack<int> &s, int mid, int i) {
+        if(mid == i) {
+            s.pop();
+            return;
+        }
+        
+        int temp = s.top();
+        s.pop();
+        
+        solve(s, mid, i+1);
+        s.push(temp);
+    }
+    
+    void deleteMid(stack<int>&s, int sizeOfStack)
+    {
+        int mid = (sizeOfStack + 1) / 2;
+        if(sizeOfStack % 2 != 0) mid--;
+        
+        solve(s, mid, 0);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int sizeOfStack;
+        cin>>sizeOfStack;
+        
+        stack<int> myStack;
+        
+        for(int i=0;i<sizeOfStack;i++)
+        {
+            int x;
+            cin>>x;
+            myStack.push(x);    
+        }
+
+            Solution ob;
+            ob.deleteMid(myStack,myStack.size());
+            while(!myStack.empty())
+            {
+                cout<<myStack.top()<<" ";
+                myStack.pop();
+            }
+        cout<<endl;
+    }   
+    return 0;
+}
+
+// } Driver Code Ends
