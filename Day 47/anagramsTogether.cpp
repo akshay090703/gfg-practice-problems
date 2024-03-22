db15461a263ca6df261835b6dd5b5c773972bd81
Akshay <akshay.pandey2021@vitbhopal.ac.in>
2024-03-22 21:25:04 +0530

    Day 47

---
+++ Day 47/anagramsTogether.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution
+{
+public:
+    vector<vector<string>> Anagrams(vector<string> &string_list)
+    {
+        int n = string_list.size();
+
+        vector<vector<string>> anagramGroups;
+        unordered_map<string, vector<string>> sortedWordsWithAnagrams;
+
+        for (int i = 0; i < n; i++)
+        {
+            string sortedWord = string_list[i];
+            sort(sortedWord.begin(), sortedWord.end());
+
+            sortedWordsWithAnagrams[sortedWord].push_back(string_list[i]);
+        }
+
+        for (auto pair : sortedWordsWithAnagrams)
+        {
+            anagramGroups.push_back(pair.second);
+        }
+
+        return anagramGroups;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<string> string_list(n);
+        for (int i = 0; i < n; ++i)
+            cin >> string_list[i];
+        Solution ob;
+        vector<vector<string>> result = ob.Anagrams(string_list);
+        sort(result.begin(), result.end());
+        for (int i = 0; i < result.size(); i++)
+        {
+            for (int j = 0; j < result[i].size(); j++)
+            {
+                cout << result[i][j] << " ";
+            }
+            cout << "\n";
+        }
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
