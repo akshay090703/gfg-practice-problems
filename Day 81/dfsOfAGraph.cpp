588dd43a154dfbdb52e402d1df6361b22fbaf136
Akshay <akshay.pandey2021@vitbhopal.ac.in>
2024-04-25 19:06:56 +0530

    Day 81

---
+++ Day 81/dfsOfAGraph.cpp
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    void dfs(vector<int> adj[], vector<int> &result, unordered_map<int, bool> &isVisited, int node)
+    {
+        if (isVisited[node])
+            return;
+
+        isVisited[node] = true;
+        result.push_back(node);
+
+        for (auto neighbour : adj[node])
+        {
+            if (!isVisited[neighbour])
+            {
+                dfs(adj, result, isVisited, neighbour);
+            }
+        }
+    }
+
+    // Function to return a list containing the DFS traversal of the graph.
+    vector<int> dfsOfGraph(int V, vector<int> adj[])
+    {
+        vector<int> result;
+        unordered_map<int, bool> isVisited;
+
+        for (int i = 0; i < V; i++)
+        {
+            if (!isVisited[i])
+            {
+                dfs(adj, result, isVisited, i);
+            }
+        }
+
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int V, E;
+        cin >> V >> E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++)
+        {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        // string s1;
+        // cin>>s1;
+        Solution obj;
+        vector<int> ans = obj.dfsOfGraph(V, adj);
+        for (int i = 0; i < ans.size(); i++)
+        {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
