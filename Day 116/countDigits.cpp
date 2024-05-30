490e6dbd69a3f54a414a80fc7c969bc347a9c705
Akshay <akshay.pandey2021@vitbhopal.ac.in>
2024-05-30 21:39:07 +0530

    Day 116

---
+++ Day 116/countDigits.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    int evenlyDivides(int N)
+    {
+        int temp = N;
+        int count = 0;
+
+        while (temp > 0)
+        {
+            int digit = temp % 10;
+            temp /= 10;
+
+            if (digit != 0 and N % digit == 0)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.evenlyDivides(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
